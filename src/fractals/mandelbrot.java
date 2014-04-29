7d3558652e8bdb2a091a91aaec2b744063896ff3
Baron Laptop <Baron Laptop@192.168.1.130>
2014-04-29 15:05:36 -0500

    getting started

---
+++ src/fractals/mandelbrot.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package fractals;
+
+import javax.swing.JApplet;
+
+/**
+ *
+ * @author Baron Laptop
+ */
+public class mandelbrot extends JApplet {
+
+    /**
+     * Initialization method that will be called after the applet is loaded into
+     * the browser.
+     */
+    @Override
+    public void init() {
+        // TODO start asynchronous download of heavy resources
+    }
+
+    // TODO overwrite start(), stop() and destroy() methods
+}
